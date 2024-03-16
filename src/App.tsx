963ff22fbb05e3fb0446015a6021bef4f1faf8c8
Zhukov Kirill <zhukovk4010@gmail.com>
2024-03-16 17:39:18 +0300

    Add prettierrc in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
 const App = () => {
-  return (
-    <>
-        <h1>Hello</h1>
-    </>
-  )
-}
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+};
 
-export default App
+export default App;
